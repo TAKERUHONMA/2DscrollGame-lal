b212b644e296cfc1bc4e7e28dfdf9b1f6429415b
武琉 本間 <230492@jc-21.jp>
2024-11-08 10:07:25 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Engine/GameObject.h"
+class Camera;
+class Field;
+
+/// <summary>
+/// プレイヤーキャラ
+/// </summary>
+class Player : public GameObject
+{
+public:
+	Player(GameObject* scene);
+	~Player();
+	void Update() override;
+	void Draw() override;
+
+	/// <summary>
+	/// プレイヤーの座標をセットする
+	/// </summary>
+	/// <param name="x">X座標</param>
+	/// <param name="y">Y座標</param>
+	void SetPosition(int x, int y);
+
+	void Reset();
+
+	int count;
+
+	bool GetReversX() { return ReversX; }
+	void ControlCollision();
+	void SetGround(float _ground) { Ground = _ground; }
+	bool MovePlayer();
+
+	bool IsActive() { return isAlive; }//isAliveがtrueかどうか
+	void DeActivateMe() { isAlive = false; }
+	void ActivateMe() { isAlive = true; }
+	void Setp_speed(float speed) { p_speed = speed; }
+	float GetMOVE_SPEED() { return MOVE_SPEED; }
+	float GetMOVE_SPEED2() { return MOVE_SPEED2; }
+
+private:
+	const float MOVE_SPEED = 2.0f;
+	const float MOVE_SPEED2 = 6.0f;
+
+	int hImage;
+	int kazu;
+
+	GameObject* sceneTop;
+	Camera* cam;
+	Field* field;
+	bool prevSpaceKey;
+	float jumpSpeed;
+	bool onGround;
+	int counter;
+	int rcount;
+	int animType; //状況
+	int animFrame;//コマ
+	int frameCounter;
+	float transparency;//透明度
+	float readyTimer;
+	bool ReversX;
+	int tmpPosx,tmpPosy;
+	int ceiling;
+	int Ground;
+	bool firstGround;
+	bool isAlive;
+	float p_speed;
+
+	enum State {
+		S_Walk=0,
+		S_Cry,
+	};
+	State state;
+};
